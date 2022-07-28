e0fe137bb65c8e9a59ef374779ebf17488e31181
Kunshothub <109427206+Kunshothub@users.noreply.github.com>
2022-07-28 15:43:43 +0900

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -1,18 +1,13 @@
-/*FAQのページ用accordion 開始*/
+var xArray = ["2017年", "2018年", "2019年", "2020年", "2021年"];
+var yArray = [13, 28, 39, 41, 52];
 
-var acc = document.getElementsByClassName("accordion");
-var i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
+var data = [{
+  x:xArray,
+  y:yArray,
+  type:"bar"
+}];
 
-/*FAQのページ用accordion 終了*/
+var layout = {title:"＜募金活動の結果＞（単位：万円）"};
+
+Plotly.newPlot("bokin", data, layout);
