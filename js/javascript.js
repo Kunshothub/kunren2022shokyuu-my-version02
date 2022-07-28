f87c958e15b2ff97de25873048b9de4df6819f7c
Kunshothub <109427206+Kunshothub@users.noreply.github.com>
2022-07-28 15:45:46 +0900

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,25 @@
+/*FAQのページ用accordion 開始*/
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+/*FAQのページ用accordion 終了*/
+
+
+/*募金*/
+
 var xArray = ["2017年", "2018年", "2019年", "2020年", "2021年"];
 var yArray = [13, 28, 39, 41, 52];
 
